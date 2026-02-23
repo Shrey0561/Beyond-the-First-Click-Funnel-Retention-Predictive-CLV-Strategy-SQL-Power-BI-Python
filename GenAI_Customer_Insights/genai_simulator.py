05bcccf16d2518bc9cf286c9c2927cc92f1190bf
Shreya Srinath <147926965+Shrey0561@users.noreply.github.com>
2026-02-23 11:58:57 +0300

    Add files via upload

---
+++ GenAI_Customer_Insights/genai_simulator.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+df = pd.read_csv("top_customers.csv")
+
+st.title("GenAI Insights Simulator - Top CLV Customers")
+
+df = pd.read_csv("top_customers.csv")
+
+st.subheader("Top CLV Customers")
+st.bar_chart(df['Predicted_CLV'])
+
+if st.button("Generate Insights"):
+   st.write("Insights for Top CLV Customers:")
+   for idx, row in df.iterrows():
+       st.write(
+           f"- Customer {row['Customer_Id']}: Likely to respond to personalized email campaigns, "
+           f"upsell potential in {row['Preferred_Category']}, "
+           f"loyalty score: {row['Loyalty_Score']}, churn risk: {row['Churn_Risk']}."
+        )
